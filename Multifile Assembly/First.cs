b3206ef5cde626f488b401778c449a226570456e
yauhenikh <khotin.13.nomer@gmail.com>
2019-08-07 18:04:46 +0300

    Add files First.cs Second.cs

---
+++ Multifile Assembly/First.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class FirstClass
+{
+	public static void Hello()
+	{
+		Console.Writeline("Hello from the FirstClass");
+	}
+}
---
+++ Multifile Assembly/Second.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class SecondClass
+{
+	public static void Hello()
+	{
+		Console.Writeline("Hello from the SecondClass");
+	}
+}
